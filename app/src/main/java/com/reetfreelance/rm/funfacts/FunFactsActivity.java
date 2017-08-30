d7079a4f23bd058aed769c53dc5181e73854c8ef
Reet Mondal <reet.mondal2@gmail.com>
2017-08-30 22:24:43 +0530

    Initial button functionality

--- app/src/main/java/com/reetfreelance/rm/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/reetfreelance/rm/funfacts/FunFactsActivity.java
@@ -2,12 +2,32 @@ package com.reetfreelance.rm.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
+    //Declare view vars
+    private TextView mFactTextView;
+    private Button mShowNewFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        mFactTextView=(TextView)findViewById(R.id.factTextView);
+        mShowNewFactButton=(Button)findViewById(R.id.showNewFactButton);
+
+        View.OnClickListener showNewFactListener=new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fact="Ostriches can run faster than horses";
+
+                mFactTextView.setText(fact);
+            }
+        };
+
+        mShowNewFactButton.setOnClickListener(showNewFactListener);
     }
 }
